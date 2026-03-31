24263863ef06bb5d5ea921c8cfbbcc592c48a606
Kanisk Dasgupta <kaniskdasgupta@gmail.com>
2026-03-31 23:49:32 +0530

    made changes for deployment

---
+++ backend/app.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from huggingface_hub import hf_hub_download
+import torch
+import json
+
+app = FastAPI()
+
+MODEL_PATH = "kanisk29/toxicity-detector-v1"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+model.eval()
+
+threshold_path = hf_hub_download(
+    repo_id=MODEL_PATH,
+    filename="thresholds.json"
+)
+
+with open(threshold_path, "r") as f:
+    thresholds = json.load(f)
+
+labels = list(thresholds.keys())
+
+class TextRequest(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(req: TextRequest):
+    inputs = tokenizer(req.text, return_tensors="pt", truncation=True, padding=True)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        probs = torch.sigmoid(outputs.logits).cpu().numpy()[0]
+
+    results = {}
+
+    for i, label in enumerate(labels):
+        prob = float(probs[i])
+        thresh = thresholds[label]
+
+        results[label] = {
+            "confidence": prob,
+            "prediction": 1 if prob >= thresh else 0
+        }
+
+    return {"results": results}
