7d12768025e202fd3744ef1b1056805023fca24d
Kanisk Dasgupta <kaniskdasgupta@gmail.com>
2026-04-07 12:13:51 +0530

    Enhanced Logging Capabilities

--- core/logging.py
+++ core/logging.py
@@ -1,13 +1,22 @@
 import logging
+import sys
+import os
 
 logger = logging.getLogger("ToxScan")
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(filename)s - %(message)s"
+)
+
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(formatter)
-file_handler = logging.FileHandler(filename="logs/app.log")
+
+file_handler = logging.FileHandler("logs/app.log")
 file_handler.setFormatter(formatter)
+
 if not logger.handlers:
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler) 
+    logger.addHandler(file_handler)
+
 logger.propagate = True
