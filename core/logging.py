51e905326f5816cacd9fe130429814a3cca2bfee
Kanisk Dasgupta <kaniskdasgupta@gmail.com>
2026-04-07 12:16:36 +0530

    Debugged Issues with Logging

--- core/logging.py
+++ core/logging.py
@@ -1,22 +1,10 @@
 import logging
 import sys
-import os
 
-logger = logging.getLogger("ToxScan")
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(filename)s - %(message)s"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
 )
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-
-file_handler = logging.FileHandler("logs/app.log")
-file_handler.setFormatter(formatter)
-
-if not logger.handlers:
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-logger.propagate = True
+logger = logging.getLogger()   # ← ROOT LOGGER
