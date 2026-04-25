eaa51ba074f9c44669267ba4fec2e6a14c60b886
Kanisk Dasgupta <kaniskdasgupta@gmail.com>
2026-04-25 11:05:20 +0530

    Added Logging

--- core/logging.py
+++ core/logging.py
@@ -1,16 +1,15 @@
 import logging
 import sys
+def get_logger(name:str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
 
-logger = logging.getLogger("kanisk29/toxicity-backend")
-logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-
-if not logger.handlers:
-    logger.addHandler(handler)
-
-logger.propagate = False
+    logger.propagate = False
+    return logger
